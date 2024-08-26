9e24546640f4faead4648fab707f27b3546e67c0
Varun Gowda MJ <104577546+varungowdamj@users.noreply.github.com>
2024-08-26 18:57:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your code here
+describe('Card Deck Test', () => {
+    it('should find the card with "Alert" and have the correct background color', () => {
+        // Check for the card with the "Alert" text and .bg-warning class
+        cy.get(".card.bg-warning").should('exist').contains("Alert");
+    });
+});
